86407e849f01fdcf14ae0da4efe685e30a977446
MirheyderA <a.mirheyder004@gmail.com>
2024-05-23 13:37:47 +0400

    some changes to the code base : 2024-23-05-13-37-46

---
+++ 23.05.2024/SwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Season");
+
+            string season = Console.ReadLine();
+
+            switch (season.ToLower())
+            {
+                case "spring":
+                    Console.WriteLine("yaz");
+                    break;
+                case "summer":
+                    Console.WriteLine("yay");
+                    break;
+                case "autumn":
+                    Console.WriteLine("payiz");
+                    break;
+                case "winter":
+                    Console.WriteLine("qis");
+                    break;
+                default:
+                    Console.WriteLine("Season is not correct");
+                    break;
+            }
+            var month = Console.ReadLine();
+            string monthAz = month switch
+            {
+                "spring" => "yaz",
+                "summer" => "yay",
+                "autumn" => "payiz",
+                "winter" => "qis"
+            };
+
+            Console.WriteLine(monthAz);
+        }
+    }
+}
