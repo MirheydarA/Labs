fd9396c6d75da76eaa2d61777a3938bc946f5fee
MirheyderA <a.mirheyder004@gmail.com>
2024-05-27 13:05:45 +0400

    some changes to the code base : 05-27-2024

---
+++ 27.05.2024/ArraysLab/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ArraysLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void FindMinElementIndex()
+        {
+            int[] array = { 0, 50, -11, 23, -18 };
+            int indexOfMinEl;
+            int min = array[0];
+            for (int i = 0; i < array.Length ; i++) 
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexOfMinEl = i;
+
+                }
+            }
+        }
+    }
+}
