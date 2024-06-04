93a4253cc54ef51671fa529f1e6797d85ad974b7
MirheyderA <a.mirheyder004@gmail.com>
2024-06-04 13:42:19 +0400

    some changes to the code base : 06-04-2024

---
+++ 04.06.2024/Function_Overload/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Function_Overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindArea(5, 6, out int area);
+            Console.WriteLine(area);
+        }
+
+        //circle
+        public static void FindArea(int r, out int area)
+        {
+            int figureArea = 3 * r * r;
+            area = figureArea;
+
+        }
+
+        //
+        public static void FindArea(int a, int b, out int area)
+        {
+            int figureArea = (a * b) / 2;
+            area = figureArea;
+        }
+    }
+}
